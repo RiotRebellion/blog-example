a51cb02ff09b1bf3d71fbe6d94340934a1658dc3
riotrebellion <misha.dzhurmiy@gmail.com>
2022-12-16 07:55:15 +0800

    fix: not correct imports

--- src/server/shared/filters/authentification-exception.filter.ts
+++ src/server/shared/filters/authentification-exception.filter.ts
@@ -1,13 +1,13 @@
 import { ArgumentsHost, Catch, ExceptionFilter } from '@nestjs/common';
-import { response } from 'express';
+import { Response, Request } from 'express';
 import { AuthentificationException } from '../utils/exceptions/authentification-exception';
 
 @Catch(AuthentificationException)
 export class AuthentificationExceptionFilter implements ExceptionFilter {
   catch(exception: AuthentificationException, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
-    const res = ctx.getResponse<Response>();
-    const req = ctx.getRequest<Request>();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
     const status = exception.getStatus();
     const message = exception.message;
 
